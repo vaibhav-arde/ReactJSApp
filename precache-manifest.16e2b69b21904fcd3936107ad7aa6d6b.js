ce51f424bf6c8a3adbedff1dd21fc9208ee67bf1
vaibhav-arde <vaibhav.arde@gmail.com>
2020-09-24 04:17:09 +0530

    Updates

---
+++ precache-manifest.16e2b69b21904fcd3936107ad7aa6d6b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6b63ae627bb9fe212f1189e150dc24",
+    "url": "/ReactJSApp/index.html"
+  },
+  {
+    "revision": "278ad7b704edf3083127",
+    "url": "/ReactJSApp/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "7f0947c2cec53a3d4c3c",
+    "url": "/ReactJSApp/static/css/main.a4c5b087.chunk.css"
+  },
+  {
+    "revision": "278ad7b704edf3083127",
+    "url": "/ReactJSApp/static/js/2.0653f16f.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/ReactJSApp/static/js/2.0653f16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0947c2cec53a3d4c3c",
+    "url": "/ReactJSApp/static/js/main.77a48aac.chunk.js"
+  },
+  {
+    "revision": "817dddb1123b29880484",
+    "url": "/ReactJSApp/static/js/runtime-main.4c68e14b.js"
+  },
+  {
+    "revision": "2c09532e5156d2dd8762d74d6798389b",
+    "url": "/ReactJSApp/static/media/Analytics.2c09532e.jpg"
+  },
+  {
+    "revision": "fcb9e5abcd73db53342288c77b50423c",
+    "url": "/ReactJSApp/static/media/AppDevlopment.fcb9e5ab.jpg"
+  },
+  {
+    "revision": "1f7f6a2dea2dd7d0f48a614af5a37e86",
+    "url": "/ReactJSApp/static/media/DigitalMarketing.1f7f6a2d.jpg"
+  },
+  {
+    "revision": "2c6d44825110ca19ecb4dd4b2e0877e8",
+    "url": "/ReactJSApp/static/media/Rocket2.2c6d4482.jpg"
+  },
+  {
+    "revision": "4c3cadef78b0030a1d3843f5ddca9fb5",
+    "url": "/ReactJSApp/static/media/SoftwareDevlopment.4c3cadef.jpg"
+  },
+  {
+    "revision": "beeb1b6c5c35d67f62f092a53dff18fc",
+    "url": "/ReactJSApp/static/media/TestAutomation2.beeb1b6c.jpg"
+  },
+  {
+    "revision": "0271289ddc216679672e1000b0c7bf87",
+    "url": "/ReactJSApp/static/media/WebDevlopment.0271289d.jpg"
+  },
+  {
+    "revision": "33a35b38dab8aa0a6e4fc3f28c77524e",
+    "url": "/ReactJSApp/static/media/deskSetup.33a35b38.jpg"
+  }
+]);
--- src/router/About.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const About = () => {
-    return (
-        <h1>I am about page</h1>
-    )
-}
-export default About;
